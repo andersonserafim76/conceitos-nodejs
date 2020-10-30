47e304c4367eff11328420363400c03253419af0
Anderson Serafim Dos Santos <anderson.serafim@totvs.com.br>
2020-10-30 11:08:52 -0300

    Desafio 1 - CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repsository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repsository);
+
+  return response.json(repsository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repsositoryIndex = repositories.findIndex(repsository => repsository.id === id);
+
+  if (repsositoryIndex < 0 ) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  const repsository = {
+    id,
+    title,
+    url, 
+    techs,
+    likes: repositories[repsositoryIndex].likes,
+  }
+
+  repositories[repsositoryIndex] = repsository;
+
+  return response.json(repsository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repsositoryIndex = repositories.findIndex(repsository => repsository.id === id);
+
+  if (repsositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repsositoryIndex, 1); //para excluir um registro do array
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repsository = repositories.find(repsository => repsository.id === id);
+
+  if (!repsository) {
+    return response.status(400).send();
+  }
+
+  repsository.likes += 1;
+
+  return response.json(repsository);
 });
 
 module.exports = app;
